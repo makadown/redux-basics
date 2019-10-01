04701e9c7f41fcf1be183602d90c66027e827a63
Makadown <maseflores79@gmail.com>
2019-09-30 19:59:46 -0500

    Acciones y reducer básico

--- app-1.ts
+++ app-1.ts
@@ -1,1 +1,32 @@
-console.log('hola mundo');
+/**
+ * Interface que representa una acción
+ */
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+/**
+ * Representa un objeto (accion) que tiene tipo 'INCREMENTAR'
+ */
+const incrementadorAction: Action = {
+    type: 'INCREMENTAR'
+};
+
+/**
+ * Un Reducer, es una funcion comun y corriente
+ * puede tener otro nombre, pero se deja así 
+ * para ilustrar.
+ * El objetivo del reducer es siempre regresr un
+ * nuevo estado.
+ * @param state 
+ * @param action 
+ */
+function reducer(state = 10, action: Action) {
+        if (action.type === 'INCREMENTAR') {
+            return state += 1;
+        }
+        return state;
+}
+
+console.log(reducer(10, incrementadorAction));
